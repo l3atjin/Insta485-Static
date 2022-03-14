bb9699e9a110b4c35cf06cbce718baab74fabe17
Batjin Lamjav <batjin@umich.edu>
2022-03-13 23:18:46 -0400

    final

--- insta485generator/__main__.py
+++ insta485generator/__main__.py
@@ -1,49 +1,70 @@
 """Build static HTML site from directory of HTML templates and plain files."""
 
-import click
-import sys
 import json
-from pathlib import Path
-from Jinja2 import Template
+import pathlib
+import shutil
+import click
+import jinja2
 
 
 @click.command()
-@click.argument('inputDir')
-@click.option('--verbose', '-v', isVerbose=True, default=False, help='Print more output.')
-@click.option('--output', '-o', nargs=1, default="html", required=True, help='Output directory.')
-
-
-def main():
+@click.argument('input_dir', required=True, type=click.Path(exists=True))
+@click.option('--verbose', '-v', is_flag=True,
+              default=False, help='Print more output.')
+@click.option('--output', '-o', nargs=1,
+              default="html", required=True, help='Output directory.')
+def main(verbose, output, input_dir):
     """Top level command line interface."""
-    print("Hello world!")
+    # print("Hello world!")
 
-    #Error message
-    if len(sys.argv) < 2:
-        print 'Error: Invalid number of arguments'
-
+    # print(input_dir)
     input_dir = pathlib.Path(input_dir)  # convert str to Path object
-    output_dir = input_dir/Path(output)  # default, can be changed with --output option
+    if output == "html":
+        output_dir = input_dir/output
+    else:
+        output_dir = pathlib.Path(output)
 
-    
-    #Might need pathlib to turn into a path
-    f = open(/input_dir/"config.json")
-    data = json.load(f)
+    template_dir = input_dir/"templates"
+    # careful here
+    # output_dir.mkdir(exist_ok=True)
 
-    for el in data:
-        url = el["url"]
+    # Might need pathlib to turn into a path
+
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(str(template_dir)),
+        autoescape=jinja2.select_autoescape(['html', 'xml']),
+    )
+
+    with open(input_dir/"config.json", "r") as content:
+        data = json.loads(content.read())
+
+    src_dir = input_dir/"static"
+    if src_dir.is_dir():
+        shutil.copytree(src_dir, output_dir)
+        # figure out the trailing /
+        if verbose:
+            print("Copied " + str(src_dir) + "/ -> " + str(output_dir) + "/")
+
+    output_dir.mkdir(exist_ok=True, parents=True)
+
+    for element in data:
+        url = element["url"]
         url = url.lstrip("/")  # remove leading slash
 
         output_path = output_dir/url
-        os.makedirs(output_path)
+        output_path.mkdir(exist_ok=True, parents=True)
 
-        template = Template(el["template"])
-        out = template.render(el["context"])
-        
-        #Rewrite this
-        text_file = open("index.html", "w")
-        text_file.write(out)
-        text_file.close()
+        template = env.get_template(element["template"])
+        out = template.render(element["context"])
 
+        if verbose:
+            print("Rendered " + element["template"] + " -> " +
+                  str(output_path/"index.html"))
+        # print(out)
+        # Rewrite this
+
+        with open(output_path/"index.html", "w") as content:
+            content.write(out)
 
 
 if __name__ == "__main__":
